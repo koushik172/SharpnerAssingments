264a69d3cc81c4a2d70d63a35e7b531b012e66f3
koushik172 <koushikrajbanshi14@gmail.com>
2023-11-09 23:48:58 +0530

    Request / Response

--- IntroductionToNodeJs/firstServer.js
+++ IntroductionToNodeJs/firstServer.js
@@ -1,8 +1,31 @@
 var http = require("http");
 
 const server = http.createServer(function (req, res) {
-  res.write("Koushik Rajbanhi");
-  res.end();
+  res.setHeader("Content-Type", "text/html");
+
+  let url = req.url;
+
+  if (url === "/node" || url === "/") {
+    res.write("<html>");
+    res.write("<head><title>Welcome to the Beginning</title></head>");
+    res.write("<body><h1>Welcome to my Node Js project</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/home") {
+    res.write("<html>");
+    res.write("<head><title>Welcome to the Beginning</title></head>");
+    res.write("<body><h1>Welcome Home</h1></body>");
+    res.write("</html>");
+    res.end();
+  } else if (url === "/about") {
+    res.write("<html>");
+    res.write("<head><title>Welcome to the Beginning</title></head>");
+    res.write("<body><h1>Welcome to About Us page</h1></body>");
+    res.write("</html>");
+    res.end();
+  }
 });
 
 server.listen(4000);
+
+console.log("http://localhost:4000");
