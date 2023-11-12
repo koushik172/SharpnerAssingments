db41eb700685826ad01bfde7c7bce040108094dc
koushik172 <koushikrajbanshi14@gmail.com>
2023-11-12 12:27:17 +0530

    Intro to Middlewares

--- Express Framework/app.js
+++ Express Framework/app.js
@@ -1,14 +1,22 @@
 import express from "express";
+import bodyParser from "body-parser";
 
 const app = express();
 
-app.use((req, res, next) => {
-  console.log("Comment 1");
-  next();
+app.use(bodyParser.urlencoded({ extended: false }));
+
+app.use("/add-product", (req, res, next) => {
+  res.send(
+    "<form action='/product' method='POST'> <input type='text' name='title'> <input type='number' name='size'> <button type='submit'>Add Product</button> </form>"
+  );
 });
 
-app.use((req, res, next) => {
-  console.log("Comment 2");
+app.post("/product", (req, res, next) => {
+  console.log(req.body.title, req.body.size);
+  res.redirect("/");
+});
+
+app.use("/", (req, res, next) => {
   res.send("<h1>Helloooo</h1>");
 });
 
