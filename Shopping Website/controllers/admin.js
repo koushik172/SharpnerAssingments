d45cff7e3557e2d56f3ecb02210a5633535749dd
koushik172 <koushikrajbanshi14@gmail.com>
2023-11-21 23:30:33 +0530

    Designed the User and Product relationship

--- Shopping Website/controllers/admin.js
+++ Shopping Website/controllers/admin.js
@@ -13,12 +13,13 @@ exports.postAddProduct = (req, res, next) => {
 	const imageUrl = req.body.imageUrl;
 	const price = req.body.price;
 	const description = req.body.description;
-	Product.create({
-		title: title,
-		imageUrl: imageUrl,
-		price: price,
-		description: description,
-	})
+	req.user
+		.createProduct({
+			title: title,
+			imageUrl: imageUrl,
+			price: price,
+			description: description,
+		})
 		.then(() => {
 			res.redirect("/");
 		})
@@ -31,9 +32,11 @@ exports.getEditProduct = (req, res, next) => {
 		return res.redirect("/");
 	}
 	const productId = req.params.productId;
-
-	Product.findByPk(productId)
-		.then((product) => {
+	req.user
+		.getProducts({ where: { id: productId } })
+		// Product.findByPk(productId)
+		.then((products) => {
+			const product = products[0];
 			if (!product) return res.redirect("/");
 			res.render("admin/edit-product", {
 				pageTitle: "Edit Product",
@@ -65,7 +68,9 @@ exports.postEditProduct = (req, res, next) => {
 };
 
 exports.getProducts = (req, res, next) => {
-	Product.findAll()
+	req.user
+		.getProducts()
+		// Product.findAll()
 		.then((products) => {
 			res.render("admin/products", {
 				prods: products,
